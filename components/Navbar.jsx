3fa62c40ec012018ad5d25c581272822d46d5f71
Tommy Thach <thachtommy5@gmail.com>
2023-05-22 08:06:43 +0700

    add  navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "../styles/Navbar.module.css";
+import Image from "next/image";
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="" width="32" height="32" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>(+84) 976297749</div>
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Product</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" alt="logo" width="140" height="140" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="cart" width="30" height="30" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
